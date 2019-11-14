24b59afa8a3c676f4a495a19059b42056ea5343c
KuznetsovSVgit <79102451200@yandex.ru>
2019-11-14 16:13:21 +0300

    w 14.11.19 16:13

---
+++ note.js
@@ -0,0 +1,83 @@
+const Note = {
+  idCounter: 8,
+  dragged: null,
+
+  process (noteElement) {
+    noteElement.addEventListener('dblclick', function (event) {
+      noteElement.removeAttribute('draggable');
+      noteElement.closest('.column').removeAttribute('draggable');
+      noteElement.setAttribute('contenteditable', 'true');
+      noteElement.focus();
+    })
+    noteElement.addEventListener('blur', function (event) {
+      noteElement.setAttribute('draggable', 'true');
+      noteElement.closest('.column').setAttribute('draggable', 'true');
+      noteElement.removeAttribute ('contenteditable');
+  
+      if (!noteElement.textContent.trim().length){
+        noteElement.remove();
+      }
+        
+    })
+    noteElement.addEventListener('dragstart', Note.dragstart)
+    noteElement.addEventListener('dragend', Note.dragend)
+    noteElement.addEventListener('dragenter', Note.dragenter)
+    noteElement.addEventListener('dragover',  Note.dragover)
+    noteElement.addEventListener('dragleave', Note.dragleave)
+    noteElement.addEventListener('drop', Note.drop)
+  },
+   
+  dragstart (event){
+    Note.dragged = this;
+    this.classList.add('dragged');
+  },
+
+  dragend (event){
+    Note.dragged = null;
+    this.classList.remove('dragged');
+    document.querySelectorAll('.note').forEach(x=>x.classList.remove('under'));
+  },
+
+  dragenter (event){
+    if (this === Note.dragged){
+      return;
+    }
+  this.classList.add('under');
+  },
+
+  dragover (event){
+    event.preventDefault();
+    if (this === Note.dragged){
+      return;
+    }
+  },
+
+  dragleave (event){
+    if (this === Note.dragged){
+      return;
+    }
+  this.classList.remove('under');
+  },
+  drop (event){
+    event.stopPropagation();
+    if (this === Note.dragged){
+      return;
+    }
+    
+    if (this.parentElement === Note.dragged.parentElement) {
+      const noteInColumn = Array.from(this.parentElement.querySelectorAll('.note'));
+      const indexA = noteInColumn.indexOf(this);
+      const indexB = noteInColumn.indexOf(Note.dragged);
+      console.log(indexA, indexB);
+      if (indexA < indexB){
+        this.parentElement.insertBefore(Note.dragged, this);
+      } else {
+        this.parentElement.insertBefore(Note.dragged, this.nextElementSibling);
+      }
+    } else {
+      this.parentElement.insertBefore(Note.dragged, this);
+    }
+  },
+
+
+}
--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let noteIdCounter = 8;
-let columnIdCounter = 3;
-
-
-
-document.querySelectorAll('.column').forEach(columnProcess);
-document.querySelectorAll('.note').forEach(noteProcess);
+document.querySelectorAll('.column').forEach(Column.process);
+document.querySelectorAll('.note').forEach(Note.process);
 
 document
   .querySelector('[data-action-addColumn]')
@@ -12,68 +7,9 @@ document
     let columnElement = document.createElement('div');
     columnElement.classList.add('column');
     columnElement.setAttribute('draggable', 'true');
-    columnElement.setAttribute('data-column-id', columnIdCounter);
+    columnElement.setAttribute('data-column-id', Column.idCounter);
     columnElement.innerHTML = '<p class="column-header" contenteditable="true">В плане</p>    <div data-notes>		</div>		<p class="column-footer">	<span data-action-addNote class="action">+ Добавить карточку</span>		</p> ';
-    columnIdCounter++;
-    columnProcess(columnElement);
+    Column.idCounter++;
+    Column.process(columnElement);
     document.querySelector('.columns').append(columnElement);
   })
-
-function columnProcess (columnElement){
-  const spanAction_addNote = columnElement.querySelector('[data-action-addNote]');
-  spanAction_addNote.addEventListener('click', function (event) {
-    let noteElement = document.createElement('div');
-    noteElement.classList.add('note');
-    noteElement.setAttribute('draggable', 'true');
-    noteElement.setAttribute('data-note-id', noteIdCounter);
-    noteIdCounter++;
-    
-    columnElement.querySelector('[data-notes]').append(noteElement); 
-    noteProcess(noteElement);   
-  })
-  const headerElement = columnElement.querySelector('.column-header');
-  headerElement.addEventListener('dblclick', function(event){
-    headerElement.setAttribute('contenteditable', 'true');
-    headerElement.focus();
-  })
-  headerElement.addEventListener('blur', function (event) {
-    headerElement.removeAttribute ('contenteditable');
-  })
-}
-
-function noteProcess (noteElement) {
-  noteElement.addEventListener('dblclick', function (event) {
-    noteElement.setAttribute('contenteditable', 'true');
-    noteElement.focus();
-  })
-  noteElement.addEventListener('blur', function (event) {
-    noteElement.removeAttribute ('contenteditable');
-  })
-
-  noteElement.addEventListener('dragstart', dragstart_noteHandler)
-  noteElement.addEventListener('dragend', dragend_noteHandler)
-  noteElement.addEventListener('dragenter', dragenter_noteHandler)
-  noteElement.addEventListener('dragover',  dragover_noteHandler)
-  noteElement.addEventListener('dragleave', dragleave_noteHandler)
-  noteElement.addEventListener('drop', drop_noteHandler)
-}
-
-
-function dragstart_noteHandler (event){
-  console.log('dragstart');
-}
-function dragend_noteHandler (event){
-  console.log('dragend');
-}
-function dragenter_noteHandler (event){
-  console.log('dragenter');
-}
-function dragover_noteHandler (event){
-  console.log('dragover');
-}
-function dragleave_noteHandler (event){
-  console.log('dragleave');
-}
-function drop_noteHandler (event){
-  console.log('drop');
-}
